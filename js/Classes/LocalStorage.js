a674f840594e53aed85f2af62f85c6733c9bb628
gabrielcaboche-bit <gabriel.caboche@3wa.io>
2025-12-15 12:26:14 +0100

    Refactor LocalStorage and Player classes for improved data handling

--- js/Classes/LocalStorage.js
+++ js/Classes/LocalStorage.js
@@ -1,54 +1,37 @@
 class LocalStorage {
   static KEY = "players";
 
-  static _getPlayers() {
+  static loadPlayers() {
     return JSON.parse(localStorage.getItem(this.KEY)) || {};
   }
 
-  static _savePlayers(players) {
+  static savePlayers(players) {
     localStorage.setItem(this.KEY, JSON.stringify(players));
   }
 
-  // Vérifier si un player existe
-  static playerExists(username) {
-    const players = this._getPlayers();
-    return !!players[username];
-  }
-
-  // Créer un nouveau player
-  static createPlayer(username) {
-    const players = this._getPlayers();
-
-    if (players[username]) {
-      throw new Error("Ce pseudo existe déjà");
-    }
-
-    players[username] = { scores: [] };
-    this._savePlayers(players);
-  }
-
-  // Charger un player existant
-  static getPlayer(username) {
-    const players = this._getPlayers();
+  static loadPlayerData(username) {
+    const players = this.loadPlayers();
     return players[username] || null;
   }
 
-  // Ajouter un score à un player
-  static addScore(username, score) {
-    const players = this._getPlayers();
-
-    if (!players[username]) {
-      throw new Error("Player introuvable");
-    }
-
-    players[username].scores.push(score);
-    this._savePlayers(players);
+  static savePlayerData(username, data) {
+    const players = this.loadPlayers();
+    players[username] = data;
+    this.savePlayers(players);
   }
 
-  // Récupérer les scores d’un player
-  static getScores(username) {
-    const players = this._getPlayers();
-    return players[username]?.scores || [];
+  static playerExists(username) {
+    const players = this.loadPlayers();
+    return username in players;
+  }
+
+  static getGlobalScores() {
+    const players = this.loadPlayers();
+    let scores = [];
+    for (const player in players) {
+      scores = scores.concat(players[player].scores);
+    }
+    return scores;
   }
 }
 
--- js/Classes/Player.js
+++ js/Classes/Player.js
@@ -1,23 +1,36 @@
 import LocalStorage from "./LocalStorage";
 
 class Player {
-  constructor(username) {
-    this._username = username;
-    this._scores = LocalStorage.getScores(username);
-  }
+    constructor(username) {
+        this._username = username;
 
-  addScore(score) {
-    this._scores.push(score);
-    LocalStorage.addScore(this._username, score);
-  }
+        if (LocalStorage.playerExists(username)) {
+            const data = LocalStorage.loadPlayerData(username);
+            this._scores = data.scores;
+        } else {
+            this._scores = [];
+            this._save(); // création en DB
+        }
+    }
 
-  get username() {
-    return this._username;
-  }
+    _save() {
+        LocalStorage.savePlayerData(this._username, {
+            scores: this._scores,
+        });
+    }
 
-  get scores() {
-    return this._scores;
-  }
+    addScore(score) {
+        this._scores.push(score);
+        this._save();
+    }
+
+    get username() {
+        return this._username;
+    }
+
+    get scores() {
+        return this._scores;
+    }
 }
 
 export default Player;
